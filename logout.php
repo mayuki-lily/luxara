9c2b79513f8dc0ad5aa304841adea3cf19e791cf
mayuki-lily <jennamayhiponia@gmail.com>
2026-04-28 01:19:21 +0800

    Simplify logout functionality in logout.php
    Removed session destruction code and simplified logout process.

--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -21,28 +20,4 @@ if (ini_get("session.use_cookies")) {
 
 header("Location: login.php");
 exit();
-=======
-<?php
-session_start();
-
-$_SESSION = [];
-
-session_destroy();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
-    );
-}
-
-header("Location: login.php");
-exit();
->>>>>>> eb79aaab1dfe4fed3e174172b57a4227c0cd9f74
 ?>
